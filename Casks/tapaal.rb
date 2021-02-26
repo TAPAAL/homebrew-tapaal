07d397a95ac62bcbcb0d88e2adb95d9d3d047025
Kenneth Yrke Jørgensen <kenneth@yrke.dk>
2021-02-26 13:12:17 +0100

    Updated tapaal to version 3.7.1 and updated ci script

--- Casks/tapaal.rb
+++ Casks/tapaal.rb
@@ -1,11 +1,12 @@
 cask "tapaal" do
-  version "3.6.1"
-  sha256 "fb2325dea82a5e2c85ca0de30d23c557e6840840cf5643ace9f38f01f2dd6419"
+  version "3.7.1"
+  sha256 "39008ce06ce3136d585a4fea61093a66c4b2de17e8f0aeb3571e64a3918d7b6d"
 
   url "http://download.tapaal.net/fileadmin/download/tapaal-#{version.major_minor}/tapaal-#{version}-mac64.dmg"
   appcast "http://www.tapaal.net/download/"
   name "TAPAAL"
-  homepage "http://www.tapaal.net/"
+  desc "Timed-Arc Petri Net editor, simulator and verification tool"
+  homepage "https://www.tapaal.net/"
 
   app "Tapaal.app"
 end
