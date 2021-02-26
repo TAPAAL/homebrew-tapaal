8b471a2ac1fe6b9edc2afb81c5e80997768c1748
Kenneth Yrke Jørgensen <kenneth@yrke.dk>
2021-02-26 10:14:50 +0100

    Updated to use " insted of ', according to style guide

--- Casks/tapaal.rb
+++ Casks/tapaal.rb
@@ -1,11 +1,11 @@
-cask 'tapaal' do
+cask "tapaal" do
   version '3.6.1'
   sha256 'fb2325dea82a5e2c85ca0de30d23c557e6840840cf5643ace9f38f01f2dd6419'
 
   url "http://download.tapaal.net/fileadmin/download/tapaal-#{version.major_minor}/tapaal-#{version}-mac64.dmg"
-  appcast 'http://www.tapaal.net/download/'
-  name 'TAPAAL'
-  homepage 'http://www.tapaal.net/'
+  appcast "http://www.tapaal.net/download/"
+  name "TAPAAL"
+  homepage "http://www.tapaal.net/"
 
-  app 'Tapaal.app'
+  app "Tapaal.app"
 end
