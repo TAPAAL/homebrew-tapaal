cc525373308e9bda836e7b7d72d5a398925d5712
Kenneth Yrke Jørgensen <kenneth@yrke.dk>
2021-04-07 12:22:17 +0200

    Update tapaal.rb

--- Casks/tapaal.rb
+++ Casks/tapaal.rb
@@ -1,9 +1,9 @@
 cask "tapaal" do
-  version "3.7.1"
-  sha256 "39008ce06ce3136d585a4fea61093a66c4b2de17e8f0aeb3571e64a3918d7b6d"
+  version "3.8.0"
+  sha256 "0CE52633FEC9AFBFB679D3DDD0A8C68F1FA8D637CDD21F441E1AB09E666D454C"
 
-  url "http://download.tapaal.net/fileadmin/download/tapaal-#{version.major_minor}/tapaal-#{version}-mac64.dmg"
-  appcast "http://www.tapaal.net/download/"
+  url "https://download.tapaal.net/tapaal/tapaal-#{version.major_minor}/tapaal-#{version}-mac64.dmg"
+  appcast "https://www.tapaal.net/download/"
   name "TAPAAL"
   desc "Timed-Arc Petri Net editor, simulator and verification tool"
   homepage "https://www.tapaal.net/"
