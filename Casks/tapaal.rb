7fa7dadd4c6a6c4a1ea3ff4a5c2cf4a268924583
Kenneth Yrke Jørgensen <kenneth@yrke.dk>
2021-11-29 11:27:18 +0100

    Update to 3.9.1

--- Casks/tapaal.rb
+++ Casks/tapaal.rb
@@ -1,6 +1,6 @@
 cask "tapaal" do
-  version "3.9.0"
-  sha256 "2E158AF9574DABB5CC1924A47178CE655984E898C785297D67EFFE5F16A0CFEB"
+  version "3.9.1"
+  sha256 "6236994E03E3E65637A47D9A29C0BBC85D375101C8760D217E209A3E7DFEB726"
 
   url "https://download.tapaal.net/tapaal/tapaal-#{version.major_minor}/tapaal-#{version}-mac64.dmg"
   appcast "https://www.tapaal.net/download/"
