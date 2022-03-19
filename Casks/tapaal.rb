198b7bcd9643eb62a3b9b1fb1304eb8f7f238851
Kenneth Yrke Jørgensen <kenneth@yrke.dk>
2022-03-19 11:22:40 +0100

    updated version to 3.9.2

--- Casks/tapaal.rb
+++ Casks/tapaal.rb
@@ -1,6 +1,6 @@
 cask "tapaal" do
-  version "3.9.1"
-  sha256 "d3689f9d9729063efe301810f4ba8dfcc4a53819c25b096a9afb0953f534699a"
+  version "3.9.2"
+  sha256 "cd825b1ad5b0a20e117775224c5bd87c558c7bbeac4fcf3d0ecfb551c2edf787"
 
   url "https://download.tapaal.net/tapaal/tapaal-#{version.major_minor}/tapaal-#{version}-mac64.dmg"
   appcast "https://www.tapaal.net/download/"
